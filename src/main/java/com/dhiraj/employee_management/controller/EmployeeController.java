fa28d5ee85c8d52b1dd39ee4b5b1ffad7e2e7598
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-07-31 19:34:00 +0530

    Completed Employee Management System CRUD operations

--- src/main/java/com/dhiraj/employee_management/controller/EmployeeController.java
+++ src/main/java/com/dhiraj/employee_management/controller/EmployeeController.java
@@ -25,4 +25,18 @@ public List<Employee> getAllEmployees() {
 public Employee getEmployeeById(@PathVariable Long id) {
     return employeeService.getEmployeeById(id);
 }
+@PutMapping("/{id}")
+public Employee updateEmployee(@PathVariable Long id,
+                               @RequestBody Employee employee) {
+
+    return employeeService.updateEmployee(id, employee);
+}
+
+@DeleteMapping("/{id}")
+public String deleteEmployee(@PathVariable Long id) {
+
+    employeeService.deleteEmployee(id);
+
+    return "Employee deleted successfully.";
+}
 }
--- src/main/java/com/dhiraj/employee_management/service/EmployeeService.java
+++ src/main/java/com/dhiraj/employee_management/service/EmployeeService.java
@@ -21,5 +21,25 @@ public class EmployeeService {
     public Employee getEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
     }
+    public Employee updateEmployee(Long id, Employee employee) {
+
+    Employee existingEmployee = employeeRepository.findById(id).orElse(null);
+
+    if (existingEmployee != null) {
+
+        existingEmployee.setName(employee.getName());
+        existingEmployee.setEmail(employee.getEmail());
+        existingEmployee.setDepartment(employee.getDepartment());
+        existingEmployee.setSalary(employee.getSalary());
+
+        return employeeRepository.save(existingEmployee);
+    }
+
+    return null;
+}
+
+public void deleteEmployee(Long id) {
+    employeeRepository.deleteById(id);
+}
 }
 
